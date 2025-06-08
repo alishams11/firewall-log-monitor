19216c659c6653b6ebfd46f9bc840a05e1cc2a9d
Ali Shams <ali.shamsodini1111@gmail.com>
2025-06-08 11:22:13 +0330

    Add alert saving to alerts.json with IP and timestamp

--- monitor/log_watcher.py
+++ monitor/log_watcher.py
@@ -1,7 +1,9 @@
 import time
 import re
 import os
+import json
 from monitor.telegram_alert import send_telegram_alert
+from datetime import datetime
 
 
 SUSPICIOUS_PATTERNS = [
@@ -17,6 +19,31 @@ def is_suspicious(line):
         if re.search(pattern, line):
             return True
     return False
+
+
+def save_alert_to_file(ip, message):
+    alert = {
+        "timestamp": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+        "ip": ip,
+        "message": message
+    }
+
+    alerts_file = "alerts/alerts.json"
+    try:
+        if not os.path.exists(alerts_file) or os.stat(alerts_file).st_size == 0:
+            alerts = []
+        else:
+            with open(alerts_file, 'r') as f:
+                alerts = json.load(f)
+
+        alerts.append(alert)
+
+        with open(alerts_file, 'w') as f:
+            json.dump(alerts, f, indent=4)
+
+    except Exception as e:
+        print(f"[!] Failed to write alert to file: {e}")
+
 
 def follow_log(file_path):
     try:
@@ -36,6 +63,9 @@ def follow_log(file_path):
                     print('\a')
                     send_telegram_alert(f"🚨 Suspicious activity detected:\n{line}")
                     os.system("mpg123 monitor/alarm.mp3 > /dev/null 2>&1")
+                    ip_match = re.search(r'SRC=(\d+\.\d+\.\d+\.\d+)', line)
+                    ip = ip_match.group(1) if ip_match else "Unknown"
+                    save_alert_to_file(ip, line)
 
                       # plays a beep sound (optional)
                     # If you have alarm.mp3:
@@ -46,3 +76,4 @@ def follow_log(file_path):
         print(f"[!] File not found: {file_path}")
     except KeyboardInterrupt:
         print("\n[!] Monitoring stopped by user.")
+    
